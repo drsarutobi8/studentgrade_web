480c41819dbf712f63e7e09e5413187961bed36b
Apichat Banyatsupasil <drsarutobi@gmail.com>
2021-12-26 02:30:01 +0700

    add update ui

--- lib/components/student_thumbnail.dart
+++ lib/components/student_thumbnail.dart
@@ -4,7 +4,7 @@ import 'package:provider/provider.dart';
 import '../grpc_stub/student.pb.dart';
 import '../models/student_manager.dart';
 
-class StudentThumbnail extends StatelessWidget {
+class StudentThumbnail extends StatefulWidget {
   final StudentReadResponse student;
 
   const StudentThumbnail({
@@ -13,34 +13,85 @@ class StudentThumbnail extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<StudentThumbnail> createState() => _StudentThumbnailState();
+}
+
+class _StudentThumbnailState extends State<StudentThumbnail> {
+  bool _editMode = false;
+  Gender? _gender;
+
+  @override
   Widget build(BuildContext context) {
+    final _nameController = TextEditingController(text: widget.student.name);
+    final _ageController = TextEditingController(
+      text: widget.student.age.toString(),
+    );
     return Container(
       padding: const EdgeInsets.all(8),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Row(children: [
-            Text(
-              student.name,
-              maxLines: 1,
-              style: Theme.of(context).textTheme.bodyText1,
-            ),
+            Visibility(
+                visible: !_editMode,
+                child: Text(
+                  widget.student.name,
+                  maxLines: 1,
+                  style: Theme.of(context).textTheme.bodyText1,
+                )),
+            Visibility(
+                visible: _editMode,
+                child: SizedBox(
+                    width: 100, child: TextField(controller: _nameController))),
             const SizedBox(width: 16.0),
-            IconButton(
-              icon: const Icon(Icons.save),
-              onPressed: () {
-                Provider.of<StudentManager>(context, listen: false)
-                    .updateStudent(student.schoolId, student.studentId,
-                        student.name, student.age, student.gender);
-              },
-            ),
+            Visibility(
+                visible: !_editMode,
+                child: IconButton(
+                  icon: const Icon(Icons.edit),
+                  onPressed: () {
+                    setState(() {
+                      _editMode = true;
+                    });
+                  },
+                )),
+            Visibility(
+                visible: _editMode,
+                child: Row(
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.save),
+                      onPressed: () {
+                        Provider.of<StudentManager>(context, listen: false)
+                            .updateStudent(
+                                widget.student.schoolId,
+                                widget.student.studentId,
+                                _nameController.value.text,
+                                int.parse(_ageController.value.text),
+                                _gender!);
+                      },
+                    ),
+                    const SizedBox(width: 16.0),
+                    IconButton(
+                      icon: const Icon(Icons.cancel),
+                      onPressed: () {
+                        setState(() {
+                          _editMode = false;
+                        });
+                      },
+                    )
+                  ],
+                )),
             const SizedBox(width: 16.0),
-            IconButton(
-              icon: const Icon(Icons.delete),
-              onPressed: () {
-                Provider.of<StudentManager>(context, listen: false)
-                    .deleteStudent(student.schoolId, student.studentId);
-              },
+            Visibility(
+              visible: !_editMode,
+              child: IconButton(
+                icon: const Icon(Icons.delete),
+                onPressed: () {
+                  Provider.of<StudentManager>(context, listen: false)
+                      .deleteStudent(
+                          widget.student.schoolId, widget.student.studentId);
+                },
+              ),
             )
           ]),
           Row(
@@ -48,20 +99,55 @@ class StudentThumbnail extends StatelessWidget {
               Text('School Id', style: Theme.of(context).textTheme.bodyText1),
               const SizedBox(width: 5.0),
               Text(
-                student.schoolId,
+                widget.student.schoolId,
               ),
               const SizedBox(width: 16.0),
               Text('Student Id', style: Theme.of(context).textTheme.bodyText1),
               const SizedBox(width: 5.0),
               Text(
-                student.studentId,
+                widget.student.studentId,
               ),
               const SizedBox(width: 16.0),
               Text('Age(years)', style: Theme.of(context).textTheme.bodyText1),
               const SizedBox(width: 5.0),
-              Text(
-                student.age.toString(),
+              Visibility(
+                visible: !_editMode,
+                child: Text(
+                  widget.student.age.toString(),
+                ),
               ),
+              Visibility(
+                  visible: _editMode,
+                  child: SizedBox(
+                      width: 100,
+                      child: TextField(
+                        controller: _ageController,
+                        maxLength: 3,
+                      ))),
+              const SizedBox(width: 16.0),
+              Text('Gender', style: Theme.of(context).textTheme.bodyText1),
+              const SizedBox(width: 5.0),
+              Visibility(
+                visible: !_editMode,
+                child: Text(
+                  widget.student.gender.toString(),
+                ),
+              ),
+              Visibility(
+                visible: _editMode,
+                child: DropdownButton<Gender>(
+                    value: widget.student.gender,
+                    onChanged: (_newValue) {
+                      setState(() {
+                        print('newValue=${_newValue}');
+                        _gender = _newValue;
+                      });
+                    },
+                    items: Gender.values
+                        .map((Gender g) =>
+                            DropdownMenuItem(value: g, child: Text(g.name)))
+                        .toList()),
+              )
             ],
           ),
         ],
